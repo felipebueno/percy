37316ad5540b5b45afdcec0ad85088196a4b6607
Felipe Bueno <bueno.felipe@gmail.com>
2021-08-31 11:54:27 -0300

    minor

--- out/extension.js
+++ out/extension.js
@@ -44,15 +44,8 @@ const toPascalCase = (str) => {
     const ret = str.replace(/(\w)(\w*)/g, function (_g0, g1, g2) { return g1.toUpperCase() + g2.toLowerCase(); });
     return ret.replace('-', '');
 };
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "percy-code" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
+    // TODO: Refactor this thing. It works but it's a mess.
     const disposable0 = vscode.commands.registerCommand(GO_TO_SCSS_COMMAND, () => {
         if (!vscode.workspace.name) {
             return;
--- src/extension.ts
+++ src/extension.ts
@@ -50,22 +50,13 @@ const toPascalCase = (str: string) => {
 	return ret.replace('-', '');
 };
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// TODO: Refactor this thing. It works but it's a mess.
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "percy-code" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable0 = vscode.commands.registerCommand(GO_TO_SCSS_COMMAND, () => {
 		if (!vscode.workspace.name) {
 			return;
 		}
-
 
 		const activeTextEditor = vscode.window.activeTextEditor;
 
